79d86691258f960e62c46d56d410c659a718c72c
Gennadi Janzen <g.janzen@checkdomain.de>
2018-04-12 19:29:43 +0200

    tmp

---
+++ bin/open-in-phpstorm.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Open the given directory in PhpStorm
+/usr/local/bin/pstorm ${1};
+
+# Shift focus to PhpStorm (macOS)
+open -a PhpStorm;
--- vim/vim/init.vim
+++
@@ -1,1 +0,0 @@
-/Users/gennadijanzen/.vimrc
