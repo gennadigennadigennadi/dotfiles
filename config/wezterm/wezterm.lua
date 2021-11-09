aa0ecc19ed6ad3fb7f773fead76de2f87ee64f48
gennadigennadigennadi <gennadijanzen@icloud.com>
2021-11-09 11:09:36 +0100

    commit changes to wezterm and fish

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -1,13 +1,13 @@
 local wezterm = require "wezterm"
 
 return {
-    font = wezterm.font "FiraCode Nerd Font Mono",
-    -- font = wezterm.font("JetBrainsMono Nerd Font Mono"),
+    -- font = wezterm.font "FiraCode Nerd Font Mono",
+    font = wezterm.font "JetBrainsMono Nerd Font Mono",
     hide_tab_bar_if_only_one_tab = true,
     font_size = 13,
-    line_height = 1.4, -- themes : "Github", "GitHub Dark", "Gruvbox Dark", "OneHalfDark",
+    line_height = 1.5, -- themes : "Github", "GitHub Dark", "Gruvbox Dark", "OneHalfDark",
     color_scheme = "nord",
-    colors = { background = "#fdf7e7" },
+    -- colors = { background = "#fdf7e7" },
     window_padding = {
         -- left = 20,
         right = 0,
