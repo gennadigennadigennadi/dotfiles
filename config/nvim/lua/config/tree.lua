e40943dadfbe641790f72730a6fc5b76021b41a2
gennadigennadigennadi <gennadijanzen@icloud.com>
2021-11-11 22:48:21 +0100

    update nvim-tree setting: change dir by projetc root

--- config/nvim/lua/config/tree.lua
+++ config/nvim/lua/config/tree.lua
@@ -1,10 +1,11 @@
+vim.g.nvim_tree_respect_buf_cwd = 1
 return require("nvim-tree").setup {
     -- disables netrw completely
     disable_netrw = true,
     -- hijack netrw window on startup
     hijack_netrw = true,
     -- open the tree when running this setup function
-    open_on_setup = true,
+    open_on_setup = false,
     -- will not open on setup if the filetype is in this list
     ignore_ft_on_setup = {},
     -- closes neovim automatically when the tree is the last **WINDOW** in the view
@@ -12,27 +13,20 @@ return require("nvim-tree").setup {
     -- opens the tree when changing/opening a new tab if the tree wasn't previously opened
     open_on_tab = false,
     -- hijack the cursor in the tree to put it at the start of the filename
-    hijack_cursor = false,
+    hijack_cursor = true,
     -- updates the root directory of the tree on `DirChanged` (when your run `:cd` usually)
-    update_cwd = false,
+    update_cwd = true,
     diagnostics = { enable = true },
     -- update the focused file on `BufEnter`, un-collapses the folders recursively until it finds the file
     update_focused_file = {
         -- enables the feature
-        enable = false,
+        enable = true,
         -- update the root directory of the tree to the one of the folder containing the file if the file is not under the current root directory
         -- only relevant when `update_focused_file.enable` is true
-        update_cwd = false,
+        update_cwd = true,
         -- list of buffer names / filetypes that will not update the cwd if the file isn't found under the current root directory
         -- only relevant when `update_focused_file.update_cwd` is true and `update_focused_file.enable` is true
         ignore_list = {},
-    },
-    -- configuration options for the system open command (`s` in the tree by default)
-    system_open = {
-        -- the command to run this, leaving nil should work in most cases
-        cmd = nil,
-        -- the command arguments as a list
-        args = {},
     },
 
     view = {
@@ -42,12 +36,5 @@ return require("nvim-tree").setup {
         side = "left",
         -- if true the tree will resize itself after opening a file
         auto_resize = true,
-        mappings = {
-            -- custom only false will merge the list with the default mappings
-            -- if true, it will only use your list to set the mappings
-            custom_only = false,
-            -- list of mappings to set on the tree manually
-            list = {},
-        },
     },
 }
