848f5de924e5c6ee965bb7025734c352667732d9
Gennadi McKelvey <gennadijanzen@icloud.com>
2023-06-22 17:04:40 +0200

    updated nix, add gui tools and wrap them via nixGl

--- flake.nix
+++ flake.nix
@@ -2,18 +2,29 @@
     description = "Home Manager configuration of gennadi";
 
     inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+        # nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+
+        nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+        # nixgl is needed for alacritty outside of nixOS
+        # refer to https://github.com/NixOS/nixpkgs/issues/122671
+        # https://github.com/guibou/nixGL/#use-an-overlay
+        nixgl.url = "github:guibou/nixGL";
 
         home-manager = {
-            url = "github:nix-community/home-manager/release-23.05";
+            url = "github:nix-community/home-manager";
             inputs.nixpkgs.follows = "nixpkgs";
         };
     };
 
-    outputs = { nixpkgs, home-manager, ... }:
+    outputs = { nixpkgs, home-manager, nixgl, ... }:
     let
         system = "x86_64-linux";
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgsForSystem = system: import nixpkgs {
+            inherit system;
+            config = { allowUnfree = true; };
+            overlays = [ nixgl.overlay ];
+      };
+       pkgs = pkgsForSystem system;
     in {
         homeConfigurations."gennadi" = home-manager.lib.homeManagerConfiguration {
             inherit pkgs;
