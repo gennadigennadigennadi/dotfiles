1ce6568880dc3289d7e56e4d1b3bb87709a78a5c
Gennadi McKelvey <gennadijanzen@icloud.com>
2023-09-29 11:33:32 +0200

    update indent-blankline.lua

--- config/nvim/lua/plugins/indent-blankline.lua
+++ config/nvim/lua/plugins/indent-blankline.lua
@@ -2,7 +2,7 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     event = "VeryLazy",
     config = function()
-        require("indent_blankline").setup({
+        require("ibl").setup({
             show_trailing_blankline_indent = false,
             char = "▏",
             filetype_exclude = { "mason", "dashboard" },
