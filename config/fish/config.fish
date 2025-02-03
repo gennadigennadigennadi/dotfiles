2b4a93086fd3514506c0f89e6a263187624c3021
Gennadi McKelvey <gennadijanzen@icloud.com>
2025-02-03 23:28:31 +0100

    set host env

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -2,6 +2,7 @@ zoxide init fish --cmd cd | source
 
 set -Ux UID (id -u)
 # set -Ux DOCKER_HOST ""
+set -Ux HOST (ipconfig getifaddr en0)
 
 set -Ux XDG_CONFIG_HOME "$HOME/.config"
 # set -Ux COLIMA_HOME "$HOME/.config/colima"
@@ -20,14 +21,19 @@ set -U fish_user_paths  $fish_user_paths
 
 abbr brewu 'brew update && brew upgrade'
 
-abbr ll 'eza -gla --icons=auto'
 abbr dc 'docker compose'
 abbr c 'composer'
 abbr mc 'mutagen-compose'
 abbr m 'make'
-abbr l 'eza -gl --icons=auto'
 abbr ip 'ipconfig getifaddr en1'
 
 abbr g 'git'
 abbr v 'nvim'
 abbr lg 'lazygit'
+
+alias l 'eza -gl --icons=auto'
+alias ll 'eza -gla --icons=auto --group-directories-first'
+
+### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
+set --export --prepend PATH "/Users/gennadi.mckelvey/.rd/bin"
+### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
